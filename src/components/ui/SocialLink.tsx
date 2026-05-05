6a943ffefc03e3dcb1c824f798addb7024454a40
ryan <ryancruse1807@gmail.com>
2026-05-05 17:54:17 +0100

    feat: accessible reusable social link component

---
+++ src/components/ui/SocialLink.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  href: string;
+  children: React.ReactNode;
+  ariaLabel: string;
+}
+
+export default function SocialLink({href, children, ariaLabel}: Props) {
+  return (
+    <li>
+      <a
+        className="transition-colors duration-500 ease-in-out hover:text-blue-500 focus-visible:outline-none focus-visible:text-blue-500"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href}
+        aria-label={ariaLabel}>
+          {children}
+      </a>
+    </li>
+
+  )
+}
