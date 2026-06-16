15766de19b36a04fc6df24a22a5477426ff0574f
ryan <ryancruse1807@gmail.com>
2026-06-16 15:31:59 +0100

    feature: Mobile navigation link component and test changes

--- src/components/other/Navigation.tsx
+++ src/components/other/Navigation.tsx
@@ -5,6 +5,8 @@ import ContactIcon from "../icons/ContactIcon";
 import MobileNavLink from "../ui/MobileNavLink";
 
 
+
+
 type NavigationProps = {
   activeSection: string;
   setActiveSection: React.Dispatch<React.SetStateAction<string>>;
@@ -12,30 +14,31 @@ type NavigationProps = {
 
 export default function Navigation({ activeSection, setActiveSection }: NavigationProps) {
   return (
-    <nav className=" bg-base-100 text-base-content">
+    <nav className=" bg-base-100 text-base-content border-b-base-300 border-b">
 
-      <div className="flex items-center justify-between p-4">
+      <div className="flex items-center justify-between p-4 container mx-auto">
 
         {/** Brand */}
-        <h1 className="text-3xl">Ryan Cruse</h1>
+        <h1 className="text-3xl underline underline-offset-8 decoration-0 decoration-primary lg:text-4xl">Ryan Cruse</h1>
 
 
         {/** Mobile Menu */}
-        <ul className="flex items-center gap-x-2">
+        <ul className="flex items-center gap-x-2 md:hidden">
 
-          <MobileNavLink section="about" activeSection={activeSection} setActiveSection={setActiveSection}>
+          <MobileNavLink section="about" activeSection={activeSection} setActiveSection={setActiveSection} ariaLabel="About">
             <AboutIcon className="size-5"/>
           </MobileNavLink>
 
-          <MobileNavLink section="projects" activeSection={activeSection} setActiveSection={setActiveSection}>
+          <MobileNavLink section="projects" activeSection={activeSection} setActiveSection={setActiveSection} ariaLabel="Projects">
             <ProjectsIcon className="size-5" />
           </MobileNavLink>
 
-          <MobileNavLink section="contact" activeSection={activeSection} setActiveSection={setActiveSection}>
+          <MobileNavLink section="contact" activeSection={activeSection} setActiveSection={setActiveSection} ariaLabel="Contact">
             <ContactIcon className="size-5" />
           </MobileNavLink>
-
         </ul>
+
+
 
 
       </div>
--- src/components/ui/MobileNavLink.tsx
+++ src/components/ui/MobileNavLink.tsx
@@ -3,23 +3,33 @@ type MobileNavLinkProps = {
   activeSection: string;
   setActiveSection: React.Dispatch<React.SetStateAction<string>>;
   children: React.ReactNode;
+  ariaLabel: string;
 };
 
-
-export default function MobileNavLink({ section, activeSection, setActiveSection, children }: MobileNavLinkProps) {
-
+export default function MobileNavLink({
+  section,
+  activeSection,
+  setActiveSection,
+  children,
+  ariaLabel
+}: MobileNavLinkProps) {
   const isLinkActive = activeSection === section;
 
   return (
     <li>
       <button
         onClick={() => setActiveSection(section)}
-        className={`p-1.5 border rounded-lg flex items-center
-          ${isLinkActive ? 'bg-primary/40 border-primary' :
-          'bg-neutral border-neutral'}`}>
+        className={`p-2 border rounded-lg flex items-center justify-center transition-all ease-in-out duration-300
+          ${isLinkActive
+            ? 'bg-primary/40 border-primary'
+            : 'bg-neutral border-neutral '
+          }
+
+        `}
+        aria-label={ariaLabel}
+        aria-current={isLinkActive ? 'page' : undefined}>
         {children}
       </button>
     </li>
-  )
-
+  );
 }
